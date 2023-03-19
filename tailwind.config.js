7b48ee2b345a583eccbca22d29429e5068140d90
ike <blazej@wodecki.dev>
2023-03-19 23:37:51 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        tetra: {
+          50: "#000000",
+          100: "#e7960b",
+          200: "#f6a00b",
+          300: "#f6970d",
+          400: "#f68e0e",
+          500: "#f57c11",
+          600: "#F56B1A",
+          700: "#f45a22",
+          800: "#f4492b",
+          900: "#f33733",
+        },
+      },
+    },
+    fontFamily: {
+      heading: ["Monument Extended", "sans-serif"],
+      sans: ["Clash Display", `sans-serif`],
+    },
+  },
+  plugins: [],
+};
