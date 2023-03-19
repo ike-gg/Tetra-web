437a5ac0cc631c0de7187028ffe88d8655b264b4
ike <blazej@wodecki.dev>
2023-03-19 23:38:58 +0100

    feat: rootlayout app wrapper

---
+++ src/components/layout/RootLayout.tsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+import { FC } from "react";
+import Navbar from "./navbar/Navbar";
+
+interface Props {
+  children: JSX.Element;
+}
+
+const RootLayout: FC<Props> = ({ children }) => {
+  return (
+    <motion.div className="max-w-screen-xl mx-auto px-4">
+      <Navbar />
+      {children}
+    </motion.div>
+  );
+};
+
+export default RootLayout;
