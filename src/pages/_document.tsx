27edba4baf62f88210ef25f3ebc35fd8e8212ea8
ike <blazej@wodecki.dev>
2023-03-19 23:39:52 +0100

    feat: app conf

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=clash-display@200,400,700,500,600,300&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.cdnfonts.com/css/monument-extended"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="bg-black text-neutral-100">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
