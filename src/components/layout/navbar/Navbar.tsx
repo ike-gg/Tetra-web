4f29fe2077b373a6cfad0762971fb2056c898953
ike <blazej@wodecki.dev>
2023-03-20 21:06:06 +0100

    feat: flex wrapper for navbar

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="sticky top-0 backdrop-blur-md z-40 bg-black/80 p-6 flex justify-between items-center">
+    <nav className="sticky top-0 backdrop-blur-md z-30 bg-black/80 p-6 flex justify-between items-center">
       <h1 className="font-heading text-4xl">Tetra</h1>
       <div className="hidden md:flex gap-6 font-normal text-lg">
         {links.map((link) => (
