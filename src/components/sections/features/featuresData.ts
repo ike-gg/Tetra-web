6074e061ad17bd16765560d8221f20e12c7f2399
ike-gg <blazej.wodecki@icloud.com>
2023-03-21 22:44:39 +0100

    feat: changed interface of feature data

--- src/components/sections/features/featuresData.ts
+++ src/components/sections/features/featuresData.ts
@@ -3,29 +3,34 @@ export interface Feature {
   height: number;
   name: string;
   shortDesc: string;
-  longDesc: string;
+  longDesc: string[];
   command?: string;
   videoUrl?: string;
-  label?: string;
+  label: string;
 }
 
 const featuresData: Feature[] = [
   {
     id: "addemotelink",
     height: 20,
+    label: "command",
     name: "Add emote (by link)",
     shortDesc: "Add emote using its identificator or link",
-    longDesc:
-      "To add emote using link from 7TV you need a reference to it, like ID or URL. It's really easy to obtain. Just visit 7tv.app page and select emote you'd like to add to your server. Bot should accept the reference in many variants, but it must include individual ID (which is included in URL's)\n\nThere's also option to copy emote reference directly from twitch chat, just right click on emote and click *Copy image address*.To add emote using link from 7TV you need a reference to it, like ID or URL. It's really easy to obtain. Just visit 7tv.app page and select emote you'd like to add to your server. Bot should accept the reference in many variants, but it must include individual ID (which is included in URL's)\n\nThere's also option to copy emote reference directly from twitch chat, just right click on emote and click *Copy image address*.To add emote using link from 7TV you need a reference to it, like ID or URL. It's really easy to obtain. Just visit 7tv.app page and select emote you'd like to add to your server. Bot should accept the reference in many variants, but it must include individual ID (which is included in URL's)\n\nThere's also option to copy emote reference directly from twitch chat, just right click on emote and click *Copy image address*.",
+    longDesc: [
+      "To add emote using link from 7TV you need a reference to it, like ID or URL. It's really easy to obtain. Just visit 7tv.app page and select emote you'd like to add to your server. Bot should accept the reference in many variants, but it must include individual ID (which is included in URL's)\n\nThere's also option to copy emote reference directly from twitch chat, just right click on emote and click *Copy image address*.",
+      "To add emote uasdfasdfeed a reference to it, like ID or URL. It's really easy to obtain. Just visit 7tv.app page and select emote you'd like to add to your server. Bot should accept the reference in many variants, but it must include individual ID (which is included in URL's)\n\nThere's also option to copy emote reference directly from twitch chat, just right click on emote and click *Copy image address*.",
+      "To add emote using lasdfit must include individual ID (which is included in URL's)\n\nThere's also option to copy emote reference directly from twitch chat, just right click on emote and click *Copy image address*.",
+    ],
     videoUrl:
       "https://cdn.discordapp.com/attachments/1087185070583980093/1087191303286181968/bylink.mp4",
   },
   {
     id: "addemotename",
     height: 16,
+    label: "command",
     name: "Add emote (by name)",
     shortDesc: "Add emote using its name",
-    longDesc: "heheh",
+    longDesc: ["heheh"],
     videoUrl:
       "https://cdn.discordapp.com/attachments/1087185070583980093/1087225203702108211/byname.mov",
   },
@@ -34,7 +39,7 @@ const featuresData: Feature[] = [
     height: 22,
     name: "TikTok",
     shortDesc: "Send plain video extracted from provided link.",
-    longDesc: "heheh",
+    longDesc: ["heheh"],
     label: "EXPERIMENTAL",
     videoUrl:
       "https://cdn.discordapp.com/attachments/1087185070583980093/1087191303286181968/bylink.mp4",
@@ -42,18 +47,20 @@ const featuresData: Feature[] = [
   {
     id: "addemotechannel",
     height: 18,
+    label: "command",
     name: "Add emote (by channel)",
     shortDesc: "Fetch 7tv set emotes from channels",
-    longDesc: "heheh",
+    longDesc: ["heheh"],
     videoUrl:
       "https://cdn.discordapp.com/attachments/1087185070583980093/1087191303286181968/bylink.mp4",
   },
   {
     id: "addsubemote",
     height: 24,
+    label: "command",
     name: "Add sub emote (twitch)",
     shortDesc: "Fetch sub emotes from twitch channel.",
-    longDesc: "heheh",
+    longDesc: ["heheh"],
     videoUrl:
       "https://cdn.discordapp.com/attachments/1087185070583980093/1087191303286181968/bylink.mp4",
   },
@@ -62,8 +69,9 @@ const featuresData: Feature[] = [
     height: 18,
     name: "Edit emotes through website",
     shortDesc: "Expand possibilites with web version of tetra",
-    longDesc:
+    longDesc: [
       "Sometimes automatically optimising just doesnt work. There are few factors about gifs and their sizes- colors, resolution and length. With Tetra you will have controll of everything.",
+    ],
     label: "coming later",
   },
 ];
