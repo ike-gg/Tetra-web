1dd56f708d8b8febec0bd13b9d6d1104d36cd521
ike-gg <blazej.wodecki@icloud.com>
2023-03-23 23:45:34 +0100

    chore: tailwind variables added

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,11 +21,22 @@ module.exports = {
         blackplus: {
           900: "#080808",
         },
+        discord: {
+          bg: "#383A40",
+          text: "#EBEEF1",
+        },
+      },
+      backgroundImage: {
+        "dotted-grid":
+          "radial-gradient(circle, #555555 1px, rgba(0, 0, 256, 0) 1px);",
+        "lined-grid":
+          "linear-gradient(to right, #222222 1px, transparent 1px),linear-gradient(to bottom, #222222 1px, transparent 1px)",
       },
     },
     fontFamily: {
       heading: ["Monument Extended", "sans-serif"],
       sans: ["Clash Display", `sans-serif`],
+      discord: ["gg sans Normal", "sans-serif"],
     },
   },
   plugins: [],
