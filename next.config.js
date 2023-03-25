0ae6a1d3b1ffcadb687c5068f667960b38f47f4f
ike-gg <blazej.wodecki@icloud.com>
2023-03-25 19:04:46 +0100

    feat: url invite redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/invite",
+        destination:
+          "https://discord.com/api/oauth2/authorize?client_id=1029899066911490158&permissions=413390915648&scope=applications.commands%20bot",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
