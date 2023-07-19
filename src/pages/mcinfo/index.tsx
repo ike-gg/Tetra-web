84a0faa6c2d0b3baa5951ec155d26d7746656124
ike <67127723+ike-gg@users.noreply.github.com>
2023-07-19 16:42:48 +0200

    feat: whitelist update

--- src/pages/mcinfo/index.tsx
+++ src/pages/mcinfo/index.tsx
@@ -5,70 +5,83 @@ import Paragraph from "@/components/ui/paragraph/Paragraph";
 
 const McInfoPage = () => {
   return (
-    <div className="p-8 my-4 border border-neutral-800 rounded-lg">
-      <Heading>IP: mc.tetra.lol</Heading>
-      <Paragraph>smiga na 1.20.1</Paragraph>
-      <hr className="my-4 border-neutral-800" />
-      <Paragraph>
-        na serwie jest voice mod, musisz go u siebie pobrac zeby korzystac z
-        voice chatu:
-      </Paragraph>
-      <Paragraph>
-        od siebie polecam launcher <b>prisma launcher</b>, zaleznie od tego czy
-        masz premke pobierasz albo oryginalnego albo zcrackowanego (zeby odpalac
-        nonpremium)
-      </Paragraph>
-      <hr className="my-4 border-neutral-800" />
-      <SubHeading>Launcher</SubHeading>
-      <div className="flex gap-4 my-3 items-center">
-        <Button className="w-fit" href="https://prismlauncher.org/download/">
-          dla premium
+    <div>
+      <div className="p-8 my-4 bg-tetra-600 text-white rounded-lg">
+        <Heading>WHITELISTA JEST WLACZONA</Heading>
+        <Paragraph>jak chcesz grac to dm (@uxw)</Paragraph>
+        <Button href="https://discordapp.com/users/224978978362884096/">
+          @uxw na discord
         </Button>
-        <Button
-          className="w-fit"
-          href="https://github.com/Diegiwg/PrismLauncher-Cracked/releases/tag/7.1"
-        >
-          dla non premium
-        </Button>
-        <span>
-          *dla non premium chyba wersja{" "}
-          <i className="text-tetra-800">
-            PrismLauncher-Windows-MinGW-w64-Setup-7.1.exe
-          </i>{" "}
-          bedzie git
-        </span>
+        user id 224978978362884096
       </div>
-      <hr className="my-4 border-neutral-800" />
-      <SubHeading>instalowanie voice moda</SubHeading>
-      <Paragraph>
-        prisma launcher umozliwia latwe instalowanie modow, wiec powinno byc to
-        banalnie proste. ruwniez polecam od sb pobranie gotowej paczki
-        &quot;fabulously optimized&quot; (daje duzo fpsow)
-      </Paragraph>
-      <Paragraph className="py-8">
-        tworzysz nowa instancje na <b>fabricu (wazne!)</b> na wersji 1.20.1,
-        wchodzisz w ustawienia instancji, zakladka mody, wyszukujesz moda o
-        nazwie <i>Simple Voice Chat</i>{" "}
-        <a
-          className="underline text-tetra-800"
-          target="_blank"
-          href="https://modrinth.com/plugin/simple-voice-chat"
-        >
-          (to ten mod)
-        </a>{" "}
-        dodajesz i tyle, powinno smigac
-      </Paragraph>
-      <Paragraph className="p-8 bg-neutral-800 rounded-lg">
-        oczywiscie forma poradnika dla srednio rozgarnietych zyciowo i
-        technicznie krok po kroku tutaj:
-        <Button className="bg-tetra-600 text-white" href="https://youtube.com">
-          poradnik dla srednio rozgarnietych zyciowo i technicznie
-        </Button>
-      </Paragraph>
-      <Heading>IP: mc.tetra.lol</Heading>
-      <Heading>IP: mc.tetra.lol</Heading>
-      <Heading>IP: mc.tetra.lol</Heading>
-      <Heading>IP: mc.tetra.lol</Heading>
+      <div className="p-8  border border-neutral-800 rounded-lg">
+        <Heading>IP: mc.tetra.lol</Heading>
+        <Paragraph>smiga na 1.20.1</Paragraph>
+        <hr className="my-4 border-neutral-800" />
+        <Paragraph>
+          na serwie jest voice mod, musisz go u siebie pobrac zeby korzystac z
+          voice chatu:
+        </Paragraph>
+        <Paragraph>
+          od siebie polecam launcher <b>prisma launcher</b>, zaleznie od tego
+          czy masz premke pobierasz albo oryginalnego albo zcrackowanego (zeby
+          odpalac nonpremium)
+        </Paragraph>
+        <hr className="my-4 border-neutral-800" />
+        <SubHeading>Launcher</SubHeading>
+        <div className="flex gap-4 my-3 items-center">
+          <Button className="w-fit" href="https://prismlauncher.org/download/">
+            dla premium
+          </Button>
+          <Button
+            className="w-fit"
+            href="https://github.com/Diegiwg/PrismLauncher-Cracked/releases/tag/7.1"
+          >
+            dla non premium
+          </Button>
+          <span>
+            *dla non premium chyba wersja{" "}
+            <i className="text-tetra-800">
+              PrismLauncher-Windows-MinGW-w64-Setup-7.1.exe
+            </i>{" "}
+            bedzie git
+          </span>
+        </div>
+        <hr className="my-4 border-neutral-800" />
+        <SubHeading>instalowanie voice moda</SubHeading>
+        <Paragraph>
+          prisma launcher umozliwia latwe instalowanie modow, wiec powinno byc
+          to banalnie proste. ruwniez polecam od sb pobranie gotowej paczki
+          &quot;fabulously optimized&quot; (daje duzo fpsow)
+        </Paragraph>
+        <Paragraph className="py-8">
+          tworzysz nowa instancje na <b>fabricu (wazne!)</b> na wersji 1.20.1,
+          wchodzisz w ustawienia instancji, zakladka mody, wyszukujesz moda o
+          nazwie <i>Simple Voice Chat</i>{" "}
+          <a
+            className="underline text-tetra-800"
+            target="_blank"
+            href="https://modrinth.com/plugin/simple-voice-chat"
+          >
+            (to ten mod)
+          </a>{" "}
+          dodajesz i tyle, powinno smigac, na dole odnosnik do poradnika
+        </Paragraph>
+        <Paragraph className="p-8 bg-neutral-800 rounded-lg">
+          oczywiscie forma poradnika dla srednio rozgarnietych zyciowo i
+          technicznie krok po kroku tutaj:
+          <Button
+            className="bg-tetra-600 text-white"
+            href="https://youtube.com"
+          >
+            poradnik dla srednio rozgarnietych zyciowo i technicznie
+          </Button>
+        </Paragraph>
+        <Heading>IP: mc.tetra.lol</Heading>
+        <Heading>IP: mc.tetra.lol</Heading>
+        <Heading>IP: mc.tetra.lol</Heading>
+        <Heading>IP: mc.tetra.lol</Heading>
+      </div>
     </div>
   );
 };
